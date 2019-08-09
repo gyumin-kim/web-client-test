2b694f81054a6ebd3065c837b3042683d5f1eef6
김규민 <gmkim93@interpark.com>
2019-08-09 15:44:40 +0900

    메소드 정리, StopWatch 추가

--- src/main/java/com/gyumin/webclienttest/controller/WebClientController.java
+++ src/main/java/com/gyumin/webclienttest/controller/WebClientController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +25,49 @@ public class WebClientController {
     @Setter
     private int serverPort = DEFAULT_PORT;
 
+    @GetMapping("/tweets-blocking")
+    public List<Tweet> getTweetsBlocking() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        log.info("Starting BLOCKING Controller!");
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Tweet>> response = restTemplate.exchange(
+                getSlowServiceUri(), HttpMethod.GET, null, new ParameterizedTypeReference<List<Tweet>>() {}
+        );
+
+        List<Tweet> result = response.getBody();
+        result.forEach(tweet -> log.info(tweet.toString()));
+
+        log.info("Exiting BLOCKING Controller!");
+
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+
+        return result;
+    }
+
+    @GetMapping("/tweets-non-blocking")
+    public Flux<Tweet> getTweetsNonBlocking() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        log.info("Starting NON-BLOCKING Controller!");
+
+        Flux<Tweet> tweetFlux = WebClient.create()
+                .get()
+                .uri(getSlowServiceUri())
+                .retrieve()
+                .bodyToFlux(Tweet.class);
+        tweetFlux.subscribe(tweet -> log.info(tweet.toString()));
+
+        log.info("Exiting NON-BLOCKING Controller!");
+
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+
+        return tweetFlux;
+    }
+
     @GetMapping("/slow-service-tweets")
     public List<Tweet> getAllTweets() throws Exception {
         Thread.sleep(2000L);
@@ -32,36 +76,6 @@ public class WebClientController {
                 new Tweet("WebClient is better", "@user2"),
                 new Tweet("Ok, both are useful", "@user3")
         );
-    }
-
-    @GetMapping("/tweets-blocking")
-    public List<Tweet> getTweetsBlocking() {
-        log.info("Starting BLOCKING Controller!");
-        final String uri = getSlowServiceUri();
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<Tweet>> response = restTemplate.exchange(
-                uri, HttpMethod.GET, null,
-                new ParameterizedTypeReference<List<Tweet>>(){});
-
-        List<Tweet> result = response.getBody();
-        result.forEach(tweet -> log.info(tweet.toString()));
-        log.info("Exiting BLOCKING Controller!");
-        return result;
-    }
-
-    @GetMapping("/tweets-non-blocking")
-    public Flux<Tweet> getTweetsNonBlocking() {
-        log.info("Starting NON-BLOCKING Controller!");
-        Flux<Tweet> tweetFlux = WebClient.create()
-                .get()
-                .uri(getSlowServiceUri())
-                .retrieve()
-                .bodyToFlux(Tweet.class);
-
-        tweetFlux.subscribe(tweet -> log.info(tweet.toString()));
-        log.info("Exiting NON-BLOCKING Controller!");
-        return tweetFlux;
     }
 
     private String getSlowServiceUri() {
